303be82402454b2a640ab6f22a6d0c68500829c4
Caroline-git <80865549+Caroline-git@users.noreply.github.com>
2021-06-16 00:02:06 -0700

    JS template

--- src/index.js
+++ src/index.js
@@ -74,6 +74,37 @@ function updateTemperature(response) {
   );
 }
 
+//forecast
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="36"
+              />
+              <br />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature max">18°</span>
+                <span class="weather-forecast-temperature min">12°</span>
+              </div>
+            </div>
+          </div>
+   `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //change celsius and fahrenheit with one click (with fake data at this point)
 //currentTemperature  celsius-link  fahrenheit-link
 
