8fc6f4367df222c8f706e587965e5eb3c41c5289
Caroline-git <80865549+Caroline-git@users.noreply.github.com>
2021-06-18 11:57:26 -0700

    display forecast data with API

--- src/index.js
+++ src/index.js
@@ -83,30 +83,39 @@ function updateTemperature(response) {
 
 //forecast
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
               <img
-                src="http://openweathermap.org/img/wn/50d@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="36"
               />
               <br />
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature max">18°</span>
-                <span class="weather-forecast-temperature min">12°</span>
+                <span class="weather-forecast-temperature max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>
          
    `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -120,6 +129,13 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 //change celsius and fahrenheit with one click (with fake data at this point)
 //currentTemperature  celsius-link  fahrenheit-link
 
